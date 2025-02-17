c8b1ca6d70d39d9da2118ef2f7550a38d0993db8
kafessizsosyalmedya <zehra.durmus@kafessizturkiye.com>
2025-02-17 17:03:19 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,3 +1,22 @@
-const address = "mailto:info@happymoons.com.tr?bcc=aksiyon2@kafessizturkiye.com"
-let link = address
-window.location.replace(link);
+const addresses = [
+    "https://maps.app.goo.gl/unbeeRVSyPxZXHhb6",
+    "https://maps.app.goo.gl/duFqF82jakr1Q3dE9",
+    "https://maps.app.goo.gl/77jGcJgGppJG5tcs8",
+    "https://maps.app.goo.gl/LdWp4m2Ai9NVZ2aN9",
+    "https://maps.app.goo.gl/9Z6qXi6zpMNMzdiTA",
+    "https://maps.app.goo.gl/dqrWaGsHuow8grHY6",
+    "https://maps.app.goo.gl/53qrTy4hb91QzFFs5",
+    "https://maps.app.goo.gl/9xS3wJLtpgbtsfbXA",
+    "https://maps.app.goo.gl/8zmrgTjQxUCaAPUa7",
+    "https://maps.app.goo.gl/L42NwaCcomd7FU6M6",
+    "https://maps.app.goo.gl/xsvkzMW5cV7EE6Wo7",
+    "https://maps.app.goo.gl/BRxPp771LDXo8Q9N7"
+    
+  
+    
+
+];
+
+const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+
+window.location.replace(address_1);
