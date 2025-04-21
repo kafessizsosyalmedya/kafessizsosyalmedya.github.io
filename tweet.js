1a343f2c373c41712837a3b6b0557491c1a4c127
kafessizsosyalmedya <zehra.durmus@kafessizturkiye.com>
2025-04-21 16:26:45 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,16 +1,13 @@
 const addresses = [
-    "https://maps.app.goo.gl/unbeeRVSyPxZXHhb6",
-    "https://maps.app.goo.gl/duFqF82jakr1Q3dE9",
-    "https://maps.app.goo.gl/77jGcJgGppJG5tcs8",
-    "https://maps.app.goo.gl/LdWp4m2Ai9NVZ2aN9",
-    "https://maps.app.goo.gl/9Z6qXi6zpMNMzdiTA",
-    "https://maps.app.goo.gl/dqrWaGsHuow8grHY6",
-    "https://maps.app.goo.gl/53qrTy4hb91QzFFs5",
     "https://maps.app.goo.gl/9xS3wJLtpgbtsfbXA",
     "https://maps.app.goo.gl/8zmrgTjQxUCaAPUa7",
     "https://maps.app.goo.gl/L42NwaCcomd7FU6M6",
     "https://maps.app.goo.gl/xsvkzMW5cV7EE6Wo7",
-    "https://maps.app.goo.gl/BRxPp771LDXo8Q9N7"
+    "https://maps.app.goo.gl/BRxPp771LDXo8Q9N7",
+    "https://maps.app.goo.gl/4Xsywe43zmB2uLif8",
+    "https://maps.app.goo.gl/nTmzYGCCRxXm2f2x9",
+    "https://maps.app.goo.gl/S1SkfcK6Hh49mKpZ6"
+    
     
   
     
